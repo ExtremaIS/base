5bf17b164396c23dd3e925e70485f65573dca3b8
Travis Cardwell <travis.cardwell@extellisys.com>
2014-02-24 17:14:53 +0900

    add Python scripts

---
+++ share/python/.base.activate.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+##############################################################################
+# base - base directory environment
+#   Python activation script
+# URL: http://www.extellisys.com/products/base
+##############################################################################
+#
+# This is a `base` activation script for Python projects.  To use it, create
+# soft links to this activation script and the accompanying deactivation
+# script as follows:
+#
+#     $ cd ~/projects/example
+#     $ ln -s /usr/local/share/base/python/.base.activate.sh
+#     $ ln -s /usr/local/share/base/python/.base.deactivate.sh
+#
+# Activate the base with `. base [label]` and deactivate the base with
+# `base_deactivate` as usual.
+#
+# This script automatically activates Python virtual environments when the
+# base is activated.  It expects Python virtual environments to be setup in
+# directories named "virtualenv-VERSION" in the project root.  If more than
+# one virtual environment is available, then script will prompt you to select
+# an environment to use.  Enter a selection by number, or just press enter to
+# accept the default.  Create a soft-link called "virtualenv" to a specific
+# version to make it load automatically, without prompting.  If there is only
+# one virtual environment available, it is loaded without prompting.
+#
+##############################################################################
+
+if [ -e "virtualenv" ] ; then
+	source "virtualenv/bin/activate"
+	python --version
+else
+	venvs=( $(ls -d virtualenv* | sort -rV) )
+	venvs_len=${#venvs[*]}
+	venvs_sel=0
+	venvs_i=0
+	if [ "$venvs_len" -gt "0" ] ; then
+		if [ "$venvs_len" -gt "1" ] ; then
+			while [ "$venvs_i" -lt "$venvs_len" ] ; do
+				echo "$venvs_i) ${venvs[$venvs_i]}"
+				let "venvs_i = $venvs_i + 1"
+			done
+			read -p "Select virtual environment [0]: " venvs_i
+			if [ "$venvs_i" != "" -a "${venvs[$venvs_i]+exists}" ] ; then
+				venvs_sel="$venvs_i"
+			fi
+		fi
+		source "${venvs[$venvs_sel]}/bin/activate"
+		python --version
+	else
+		echo "warning: no virtual environment loaded" >&2
+	fi
+fi
---
+++ share/python/.base.deactivate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+##############################################################################
+# base - base directory environment
+#   Python deactivation script
+# URL: http://www.extellisys.com/products/base
+##############################################################################
+#
+# This is a `base` deactivation script for Python projects.  To use it, create
+# soft links to the accompanying activation script and this deactivation
+# script as follows:
+#
+#     $ cd ~/projects/example
+#     $ ln -s /usr/local/share/base/python/.base.activate.sh
+#     $ ln -s /usr/local/share/base/python/.base.deactivate.sh
+#
+# Activate the base with `. base [label]` and deactivate the base with
+# `base_deactivate` as usual.
+#
+# This script automatically deactivates Python virtual environments when the
+# base is deactivated.
+#
+##############################################################################
+
+declare -F "deactivate" >/dev/null
+if [ "$?" -eq "0" ] ; then
+	deactivate
+fi
